ed5776ec3c36d9c2883b8742295958227502966d
Ogbonna Destiny <80477923+DestinyOgbonna@users.noreply.github.com>
2024-09-05 10:36:17 +0100

    file renaming

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,43 +1,39 @@
-<<<<<<< HEAD
 import 'package:greenvoice/src/features/authentication/register/presentation/register.dart';
-import 'package:greenvoice/utils/constants/exports.dart';
-=======
-import 'package:greenvoice/routes/app_router.dart';
 import 'package:greenvoice/src/features/issues/views/issue_description.dart';
 import 'package:greenvoice/src/features/issues/views/issues_home.dart';
 import 'package:greenvoice/src/models/user/issue/issue_model.dart';
-import 'package:greenvoice/utils/helpers/locator.dart';
+import 'package:greenvoice/utils/constants/exports.dart';
 
 export 'package:go_router/go_router.dart';
->>>>>>> c59b887 (Issue Home)
 
 final greenVoice = locator<GoRouter>();
 
 class GreenVoiceRoutes {
   final greenVoiceRouter = GoRouter(routes: [
     GoRoute(
-      path: '/',
+        path: '/',
+        builder: (context, state) => const IssuesScreen(),
+        // builder: (context, state) => const SignIn(),
+        routes: [
+          GoRoute(
+            path: 'issues',
+            builder: (context, state) => const IssuesScreen(),
+          ),
+          GoRoute(
+            path: 'issues-details',
+            builder: (context, state) => IssueDetailScreen(
+              arguments: state.extra as IssueDetailArguments,
+            ),
+          ),
+        ]),
+    GoRoute(
+      path: AppRoutes.login,
       builder: (context, state) => const Login(),
     ),
     GoRoute(
       path: AppRoutes.register,
       builder: (context, state) => const Register(),
-      builder: (context, state) => const IssuesScreen(),
-      // builder: (context, state) => const SignIn(),
-      routes: [
-        GoRoute(
-          path: 'issues',
-          builder: (context, state) => const IssuesScreen(),
-        ),
-        GoRoute(
-          path: 'issues-details',
-          builder: (context, state) => IssueDetailScreen(
-            arguments: state.extra as IssueDetailArguments,
-          ),
-        ),
-
-      ]
-    )
+    ),
   ]);
 }
 
