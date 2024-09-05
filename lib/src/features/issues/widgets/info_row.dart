7facbbb19f728475b8bafdfb6552f3ace4de4c7a
Jeremiah Oluwaseun Erinola <seunjeremiah@gmail.com>
2024-09-05 14:14:03 +0100

    Issue Description

---
+++ lib/src/features/issues/widgets/info_row.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class InfoRow extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const InfoRow({super.key, required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        children: [
+          Icon(icon, size: 16, color: Colors.grey[600]),
+          const SizedBox(width: 8),
+          Expanded(
+              child: Text(text, style: TextStyle(color: Colors.grey[600]))),
+        ],
+      ),
+    );
+  }
+}
