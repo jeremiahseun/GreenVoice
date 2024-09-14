c56faef87a5d47b52500199deb281d4b1aca1528
Jeremiah Oluwaseun Erinola <seunjeremiah@gmail.com>
2024-09-14 14:30:31 +0100

    Update bottom_navigation.dart

--- lib/src/features/bottom_navigation/presentation/bottom_navigation.dart
+++ lib/src/features/bottom_navigation/presentation/bottom_navigation.dart
@@ -2,20 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:greenvoice/src/features/bottom_navigation/data/bottom_navigation_notifier.dart';
 import 'package:greenvoice/src/features/issues/presentation/issues_home.dart';
-
-import 'package:greenvoice/src/features/projects/presentation/projects.dart';
 import 'package:greenvoice/src/features/profile/presentation/profile_view.dart';
-
-
-
+import 'package:greenvoice/src/features/projects/presentation/projects.dart';
 
 class HomeScreen extends ConsumerWidget {
   final List<Widget> _screens = [
     const IssuesView(),
-
     const ProjectHome(),
-
-
     const ProfileView()
   ];
 
@@ -36,9 +29,9 @@ class HomeScreen extends ConsumerWidget {
           ref.read(bottomNavigationProvider.notifier).setIndex(index);
         },
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Screen 1'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Projects'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Screen 3'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
     );
